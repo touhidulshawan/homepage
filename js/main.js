b327d05505993ea5de60d3bb77f8931e1be3a659
Touhidul Shawan <touhidulshawan@gmail.com>
2023-01-01 01:15:36 +0600

    add bar to search

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const submitQuery = evt => {
+    evt.preventDefault()
+    let userInput = document.getElementById("inputBar").value
+    window.location.replace(`https://google.com/search?q=${userInput}`)
+}
