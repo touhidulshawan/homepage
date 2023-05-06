b693bc8219e3288668f4fc174c3d03410b54aa23
Touhidul Shawan <touhidulshawan@gmail.com>
2023-05-06 19:59:01 +0600

    style changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const submitQuery = evt => {
-    evt.preventDefault()
-    let userInput = document.getElementById("inputBar").value
-    window.location.replace(`https://google.com/search?q=${userInput}`)
-}
+const submitQuery = (evt) => {
+  evt.preventDefault();
+  let userInput = document.getElementById("inputBar").value;
+  window.location.replace(`https://google.com/search?q=${userInput}`);
+};
