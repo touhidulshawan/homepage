da4fcad3d7ebe52fc9bf731f0ab446c4a3756e53
Touhidul Shawan <touhidulshawan@gmail.com>
2023-05-12 16:06:07 +0600

    search bar to url bar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const submitQuery = (evt) => {
   evt.preventDefault();
-  let userInput = document.getElementById("inputBar").value;
-  window.location.replace(`https://google.com/search?q=${userInput}`);
+  let userInput = document.getElementById("urlBar").value;
+  const url = `https://${userInput}`
+  /* window.location.replace(`https://google.com/search?q=${userInput}`); */
+    window.location.href = url
 };
