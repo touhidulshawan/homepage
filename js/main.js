31cb95b028c120792fce46752abccdf470b53a4c
Touhidul Shawan <touhidulshawan@gmail.com>
2024-03-09 18:10:41 +0600

    url to search

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 const submitQuery = (evt) => {
   evt.preventDefault();
   let userInput = document.getElementById("urlBar").value;
-  const url = `https://${userInput}`
-  /* window.location.replace(`https://google.com/search?q=${userInput}`); */
-    window.location.href = url
+  window.location.replace(`https://google.com/search?q=${userInput}`);
 };
